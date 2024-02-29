167c0f3bce42d5bfbf5fb992d1771d8c3dfa1dc9
Suvan Gowri Shanker <118198968+greeenboi@users.noreply.github.com>
2024-02-29 19:19:58 +0530

    Update supabase.ts
    hiding api keys

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://eedplvopkhwuhhquagfw.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlZHBsdm9wa2h3dWhocXVhZ2Z3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcyNDM1ODUsImV4cCI6MjAyMjgxOTU4NX0.uXlL7xAorEiCd_kmbZ0v3hgB0FR5vskjCHLveoATQ6g'
+const supabaseUrl = ''
+const supabaseAnonKey = ''
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
