4070cb3fda0e874e707da9d256f28f01b4638735
VijayMakkad <vijaymakkad0104@gmail.com>
2024-10-13 22:05:48 +0530

    Home and layout

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -6,9 +6,9 @@ import { ScreenContent } from '~/components/ScreenContent';
 export default function Home() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Tab One' }} />
+      <Stack.Screen options={{ title: 'Tab Two' }} />
       <Container>
-        <ScreenContent path="app/(drawer)/(tabs)/index.tsx" title="Tab One" />
+        <ScreenContent path="app/(tabs)/two.tsx" title="Tab Two" />
       </Container>
     </>
   );
